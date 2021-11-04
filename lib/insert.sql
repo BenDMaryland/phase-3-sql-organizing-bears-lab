fd2883fbf201b9b9ed9ebecb26364fd79877669d
ben.d.maryland@gmail.com <bend@RoseFan.localdomain>
2021-11-04 12:32:02 -0400

    commit tree e918df2397d31264f5a85cc81de9b779d1fd4ab3

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1,"Mr. Chocolate", 2, "M", "brown", "calm", 1);
+INSERT INTO bears VALUES (2,"Rowdy", 12, "M", "white", "rowdy", 0);
+INSERT INTO bears VALUES (3,"Tabitha", 9, "F", "black", "nice", 1);
+INSERT INTO bears VALUES (4,"Sergeant Brown", 20, "M", "brown", "reserved", 1);
+INSERT INTO bears VALUES (5,"Melissa", 2, "F", "Red???", "angry", 1);
+INSERT INTO bears VALUES (6,"Grinch", 22, "M", "purple", "selfless", 1);
+INSERT INTO bears VALUES (7,"Wendy", 21, "M", "red", "calm", 1);
+INSERT INTO bears VALUES (8,NULL, 2, "F", "brown", "calm", 1);
